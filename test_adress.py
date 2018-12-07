c921af729a014f1747ec900fa58091eef64082ec
Iulian <iuliangaluz@gmail.com>
2018-12-07 12:45:00 +0200

    Передача параметров в методы (test_adress_py)

--- test_adress.py
+++ test_adress.py
@@ -15,11 +15,30 @@ class TestAdressPy(unittest.TestCase):
     def test_adress_py(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.add_new(wd)
-        self.open_and_init_form_adress_book(wd)
+        self.open_and_init_form_adress_book(wd, firstname="gfhg", middlename="gfhf", lastname="fghfgh",
+                                  nickname="fghfgh", title="fghfgh", company="fghfgh", address="fghfgh",
+                                  home="fghfgh", mobile="fghfgh", work="fghfgh", fax="fghfgh", email="fghfgh",
+                                  email2="fghfg", email3="fghfgh", homepage="fghfgh", address2="fghfg",
+                                  phone2="fghfgh", notes="fghfgh")
         self.return_to_home_page(wd)
         self.logout(wd)
+
+        def test_adress_empty_py(self):
+            wd = self.wd
+            self.open_home_page(wd)
+            self.login(wd, username="admin", password="secret")
+            self.add_new(wd)
+            self.open_and_init_form_adress_book(wd, firstname="", middlename="", lastname="",
+                                                nickname="", title="", company="", address="",
+                                                home="", mobile="", work="", fax="",
+                                                email="",
+                                                email2="", email3="", homepage="", address2="",
+                                                phone2="", notes="")
+            self.return_to_home_page(wd)
+            self.logout(wd)
+
 
     def logout(self, wd):
         # logout
@@ -30,55 +49,57 @@ class TestAdressPy(unittest.TestCase):
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
 
-    def open_and_init_form_adress_book(self, wd):
+    def open_and_init_form_adress_book(self, wd, firstname, middlename, lastname, nickname, title, company, address,
+                                       home, mobile, work, fax, email, email2, email3, homepage, address2, phone2,
+                                       notes):
         # open and init form adress book
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("gfhg")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("gfhf")
+        wd.find_element_by_name("middlename").send_keys(middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("fghfgh")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("fghfgh")
+        wd.find_element_by_name("nickname").send_keys(nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("fghfgh")
+        wd.find_element_by_name("title").send_keys(title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("fghfgh")
+        wd.find_element_by_name("company").send_keys(company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("fghfgh")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Address:'])[1]/following::label[1]").click()
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("fghfgh")
+        wd.find_element_by_name("home").send_keys(home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("fghfgh")
+        wd.find_element_by_name("mobile").send_keys(mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("fghfgh")
+        wd.find_element_by_name("work").send_keys(work)
         wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").send_keys("fghfgh")
+        wd.find_element_by_name("fax").send_keys(fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("fghfgh")
+        wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("fghfg")
+        wd.find_element_by_name("email2").send_keys(email2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("fghfgh")
+        wd.find_element_by_name("email3").send_keys(email3)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("fghfgh")
+        wd.find_element_by_name("homepage").send_keys(homepage)
         wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
         wd.find_element_by_name("bmonth").click()
@@ -95,25 +116,25 @@ class TestAdressPy(unittest.TestCase):
         wd.find_element_by_name("ayear").send_keys("1989")
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("fghfg")
+        wd.find_element_by_name("address2").send_keys(address2)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("fghfgh")
+        wd.find_element_by_name("phone2").send_keys(phone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("fghfgh")
+        wd.find_element_by_name("notes").send_keys(notes)
 
     def add_new(self, wd):
         # add new
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
