5b3ae014f864c8518635fed41648644d34230f67
Iulian <iuliangaluz@gmail.com>
2018-12-13 16:05:19 +0200

    4 задание, Контакты(adressbook)перевел с unittest на py.test (фикстуры)

---
+++ test_adress.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from adress import Adress
+from application import Application
+
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_adress_py(app):
+        app.open_home_page()
+        app.login(username="admin", password="secret")
+        app.add_new()
+        app.open_and_init_form_adress_book (Adress(firstname="gfhg", middlename="gfhf", lastname="fghfgh",
+                                  nickname="fghfgh", title="fghfgh", company="fghfgh", address="fghfgh",
+                                  home="fghfgh", mobile="fghfgh", work="fghfgh", fax="fghfgh", email="fghfgh",
+                                  email2="fghfg", email3="fghfgh", homepage="fghfgh", address2="fghfg",
+                                  phone2="fghfgh", notes="fghfgh"))
+        app.return_to_home_page()
+        app.logout()
+
+def test_adress_empty_py(app):
+            app.open_home_page()
+            app.login(username="admin", password="secret")
+            app.add_new()
+            app.open_and_init_form_adress_book(Adress(firstname="", middlename="", lastname="",
+                                                nickname="", title="", company="", address="",
+                                                home="", mobile="", work="", fax="",
+                                                email="",
+                                                email2="", email3="", homepage="", address2="",
+                                                phone2="", notes=""))
+            app.return_to_home_page()
+            app.logout()
