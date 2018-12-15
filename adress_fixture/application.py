079fec180eaf70987ccb758ab92d919d5c3b7a84
Iulian <iuliangaluz@gmail.com>
2018-12-15 19:06:11 +0200

    Выделен класс для работы с группами

--- adress_fixture/application.py
+++ adress_fixture/application.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.support.select import Select
 from fixture.session import SessionHelper
-
+from fixture.group import GroupHelper
 
 class Application:
 
@@ -9,101 +8,12 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
             # open home page
             wd = self.wd
             wd.get("http://localhost/addressbook/index.php")
-
-
-
-    def add_new(self):
-            # add new
-            wd = self.wd
-            wd.find_element_by_link_text("add new").click()
-
-    def open_and_init_form_adress_book(self, adress):
-            # open and init form adress book
-            wd = self.wd
-            wd.find_element_by_name("firstname").click()
-            wd.find_element_by_name("firstname").clear()
-            wd.find_element_by_name("firstname").send_keys(adress.firstname)
-            wd.find_element_by_name("middlename").click()
-            wd.find_element_by_name("middlename").clear()
-            wd.find_element_by_name("middlename").send_keys(adress.middlename)
-            wd.find_element_by_name("lastname").click()
-            wd.find_element_by_name("lastname").clear()
-            wd.find_element_by_name("lastname").send_keys(adress.lastname)
-            wd.find_element_by_name("nickname").click()
-            wd.find_element_by_name("nickname").clear()
-            wd.find_element_by_name("nickname").send_keys(adress.nickname)
-            wd.find_element_by_name("title").click()
-            wd.find_element_by_name("title").clear()
-            wd.find_element_by_name("title").send_keys(adress.title)
-            wd.find_element_by_name("company").click()
-            wd.find_element_by_name("company").clear()
-            wd.find_element_by_name("company").send_keys(adress.company)
-            wd.find_element_by_name("address").click()
-            wd.find_element_by_name("address").clear()
-            wd.find_element_by_name("address").send_keys(adress.address)
-            wd.find_element_by_xpath(
-                "(.//*[normalize-space(text()) and normalize-space(.)='Address:'])[1]/following::label[1]").click()
-            wd.find_element_by_name("home").click()
-            wd.find_element_by_name("home").click()
-            wd.find_element_by_name("home").clear()
-            wd.find_element_by_name("home").send_keys(adress.home)
-            wd.find_element_by_name("mobile").click()
-            wd.find_element_by_name("mobile").clear()
-            wd.find_element_by_name("mobile").send_keys(adress.mobile)
-            wd.find_element_by_name("work").click()
-            wd.find_element_by_name("work").clear()
-            wd.find_element_by_name("work").send_keys(adress.work)
-            wd.find_element_by_name("fax").click()
-            wd.find_element_by_name("fax").send_keys(adress.fax)
-            wd.find_element_by_name("email").click()
-            wd.find_element_by_name("email").clear()
-            wd.find_element_by_name("email").send_keys(adress.email)
-            wd.find_element_by_name("email2").click()
-            wd.find_element_by_name("email2").clear()
-            wd.find_element_by_name("email2").send_keys(adress.email2)
-            wd.find_element_by_name("email3").click()
-            wd.find_element_by_name("email3").clear()
-            wd.find_element_by_name("email3").send_keys(adress.email3)
-            wd.find_element_by_name("homepage").click()
-            wd.find_element_by_name("homepage").clear()
-            wd.find_element_by_name("homepage").send_keys(adress.homepage)
-            wd.find_element_by_name("bday").click()
-            Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
-            wd.find_element_by_name("bmonth").click()
-            Select(wd.find_element_by_name("bmonth")).select_by_visible_text("February")
-            wd.find_element_by_name("byear").click()
-            wd.find_element_by_name("byear").clear()
-            wd.find_element_by_name("byear").send_keys("1986")
-            wd.find_element_by_name("aday").click()
-            Select(wd.find_element_by_name("aday")).select_by_visible_text("3")
-            wd.find_element_by_name("amonth").click()
-            Select(wd.find_element_by_name("amonth")).select_by_visible_text("April")
-            wd.find_element_by_name("ayear").click()
-            wd.find_element_by_name("ayear").clear()
-            wd.find_element_by_name("ayear").send_keys("1989")
-            wd.find_element_by_name("address2").click()
-            wd.find_element_by_name("address2").clear()
-            wd.find_element_by_name("address2").send_keys(adress.address2)
-            wd.find_element_by_name("theform").click()
-            wd.find_element_by_name("phone2").click()
-            wd.find_element_by_name("phone2").clear()
-            wd.find_element_by_name("phone2").send_keys(adress.phone2)
-            wd.find_element_by_name("notes").click()
-            wd.find_element_by_name("notes").clear()
-            wd.find_element_by_name("notes").send_keys(adress.notes)
-
-    def return_to_home_page(self):
-            # return to home page
-            wd = self.wd
-            wd.find_element_by_xpath(
-                "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
-
-
 
     def destroy(self):
         self.wd.quit()
--- adress_test/test_adress.py
+++ adress_test/test_adress.py
@@ -14,7 +14,7 @@ def test_adress_py(app):
         app.open_home_page()
         app.session.login(username="admin", password="secret")
         app.add_new()
-        app.open_and_init_form_adress_book (Adress(firstname="gfhg", middlename="gfhf", lastname="fghfgh",
+        app.adress.create (Adress(firstname="gfhg", middlename="gfhf", lastname="fghfgh",
                                   nickname="fghfgh", title="fghfgh", company="fghfgh", address="fghfgh",
                                   home="fghfgh", mobile="fghfgh", work="fghfgh", fax="fghfgh", email="fghfgh",
                                   email2="fghfg", email3="fghfgh", homepage="fghfgh", address2="fghfg",
@@ -26,11 +26,11 @@ def test_adress_empty_py(app):
             app.open_home_page()
             app.session.login(username="admin", password="secret")
             app.add_new()
-            app.open_and_init_form_adress_book(Adress(firstname="", middlename="", lastname="",
-                                                nickname="", title="", company="", address="",
-                                                home="", mobile="", work="", fax="",
-                                                email="",
-                                                email2="", email3="", homepage="", address2="",
-                                                phone2="", notes=""))
+            app.adress.create(Adress(firstname="", middlename="", lastname="",
+                                     nickname="", title="", company="", address="",
+                                     home="", mobile="", work="", fax="",
+                                     email="",
+                                     email2="", email3="", homepage="", address2="",
+                                     phone2="", notes=""))
             app.return_to_home_page()
             app.session.logout()
