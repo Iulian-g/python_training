6f7b87d90e23e06ad58b4e960e610305315aac4b
Iulian <iuliangaluz@gmail.com>
2018-12-16 11:23:12 +0200

    Создание и запуск файла conftest

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-
-
-@pytest.fixture
-def app(request):
-        fixture = Application()
-        request.addfinalizer(fixture.destroy)
-        return fixture
 
 
 def test_add_group_py(app):
         app.session.login(username="admin", password="secret")
         app.group.create(Group(name="new", header="new", footer="new"))
         app.session.logout()
-
 
 def test_add_empty_group_py(app):
         app.session.login(username="admin", password="secret")
