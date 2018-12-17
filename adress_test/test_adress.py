e03eaefef4c04591f98fd6c165f32771bd3a4055
Iulian <iuliangaluz@gmail.com>
2018-12-17 22:29:45 +0200

    Переделал test_adress.py, но не идет test_del_adress (как подтвердить и прописать всплывающее окно подтверждение ок, для удаления?)

--- adress_test/test_adress.py
+++ adress_test/test_adress.py
@@ -1,36 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-from adress_model.adress import Adress
-from adress_fixture.application import Application
+from adress_model.contact import Contact
 
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_adress_py(app):
-        app.open_home_page()
         app.session.login(username="admin", password="secret")
-        app.add_new()
-        app.adress.create(Adress(firstname="gfhg", middlename="gfhf", lastname="fghfgh",
+        app.contact.create(Contact(firstname="gfhg", middlename="gfhf", lastname="fghfgh",
                                   nickname="fghfgh", title="fghfgh", company="fghfgh", address="fghfgh",
                                   home="fghfgh", mobile="fghfgh", work="fghfgh", fax="fghfgh", email="fghfgh",
                                   email2="fghfg", email3="fghfgh", homepage="fghfgh", address2="fghfg",
                                   phone2="fghfgh", notes="fghfgh"))
-        app.return_to_home_page()
         app.session.logout()
 
 def test_adress_empty_py(app):
-            app.open_home_page()
-            app.session.login(username="admin", password="secret")
-            app.add_new()
-            app.adress.create(Adress(firstname="", middlename="", lastname="",
+        app.session.login(username="admin", password="secret")
+        app.contact.create(Contact(firstname="", middlename="", lastname="",
                                      nickname="", title="", company="", address="",
                                      home="", mobile="", work="", fax="",
                                      email="",
                                      email2="", email3="", homepage="", address2="",
                                      phone2="", notes=""))
-            app.return_to_home_page()
-            app.session.logout()
+        app.session.logout()
