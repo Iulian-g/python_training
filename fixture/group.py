5f92d57b5600e05dcc3ecfac2ac0a8ca0ef5548a
Iulian <iuliangaluz@gmail.com>
2019-01-05 13:38:23 +0200

    Задание №10: оптимизировать переходы между страницами

--- fixture/group.py
+++ fixture/group.py
@@ -8,17 +8,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create(self, group):
-        wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        self.fill_group_form(group)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
+        if not (wd.current_url.endswitch("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def change_field_value(self, field_name, text):
          wd = self.app.wd
@@ -34,12 +25,22 @@ class GroupHelper:
         self.change_field_value("group_header", group.header)
         self.change_field_value("group_footer", group.footer)
 
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click( )
 
 
-    def delete_first(self):
+    def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
