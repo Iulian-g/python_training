8a0f13d1fa60b5bb5f9bdb39f3fd2433a3df2770
Iulian <iuliangaluz@gmail.com>
2019-01-06 10:40:23 +0200

    Добавление проверки списка групп (add, del, modify)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,10 +3,18 @@ from model.group import Group
 
 
 def test_add_group_py(app):
+        old_group = app.group.get_group_list( )
         app.group.create(Group(name="new name", header="new header", footer="new footer"))
+        new_group = app.group.get_group_list( )
+        assert len(old_group) + 1 == len(new_group)
+
 
 def test_add_empty_group_py(app):
+        old_group = app.group.get_group_list( )
         app.group.create(Group(name="", header="", footer=""))
+        new_group = app.group.get_group_list( )
+        assert len(old_group) + 1 == len(new_group)
+
 
 
 
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,5 +4,8 @@ from model.group import Group
 def test_delete_first_group_py(app):
     if app.group.count == 0:
         app.group.create(Group(name="test"))
+    old_group = app.group.get_group_list( )
     app.group.delete_first_group()
+    new_group = app.group.get_group_list( )
+    assert len(old_group) - 1 == len(new_group)
 
