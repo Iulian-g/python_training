4a1fd2a155f2205c14c1da8da09e92776d310a5a
Iulian <iuliangaluz@gmail.com>
2019-01-13 20:58:53 +0200

    Случайная модификация для групп (13 задание)

--- fixture/group.py
+++ fixture/group.py
@@ -55,15 +55,17 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cash = None
 
+    def modify_first_group(self, new_group_data):
+        self.modify_group_by_index(0, new_group_data)
 
-    def modify_first_group(self, new_group_date):
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page( )
-        self.select_first_group( )
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form
-        self.fill_group_form(new_group_date)
+        self.fill_group_form(new_group_data)
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
