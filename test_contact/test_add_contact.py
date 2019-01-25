24a7adac7536cc80e2f1e71d0c1beea81cd7ea94
Iulian <iuliangaluz@gmail.com>
2019-01-25 16:31:26 +0200

    Исправления по заданию 14 1.Почти всё верно, только для емейлов тоже надо использовать метод merge_emails_like_on_homepage вместо contact_from_edit_page.all_emails_from_home_page. По примеру как это сделано для телефонов. 2.И self.all_phones_from_home_page, self.all_emails_from_home_page, self.address надо убрать из метода сравнения.

--- test_contact/test_add_contact.py
+++ test_contact/test_add_contact.py
@@ -6,7 +6,7 @@ import random
 import string
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", homephone="", mobilephone="", workphone="", fax="",
