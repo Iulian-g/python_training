b7b61c76074bcd7f0d6752843ce56b53cd4d6c27
Iulian <iuliangaluz@gmail.com>
2019-02-03 15:54:38 +0200

    Задание №17: вторая часть: Автоматическое определение путей к файлам относительно директории проекта

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 
 import pytest
-from fixture.application import Application
+import os.path
 import json
+from fixture.application import Application
+
 
 fixture = None
 target = None
+
 
 @pytest.fixture
 def app(request):
@@ -12,8 +15,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target['base_url'])
     fixture.session.ensure_login(username=target['username'], password=target['password'])
